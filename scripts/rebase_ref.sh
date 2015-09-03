ebf79297b38722d9def8a4e27ed7b76a49b1977a
Lucas <ljacobson64@gmail.com>
2015-09-03 17:59:44 -0500

    fixed rebase_ref.sh

--- scripts/rebase_ref.sh
+++ scripts/rebase_ref.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd Dagmc
+cd ../Dagmc
 
 for i in $(seq -f "%02g" 1 15); do
   cp results/$i/test$i.o cases/$i/ref.dag.out
   cp results/$i/test$i.m cases/$i/ref.dag.mctal
 done
 
-cd ..
+cd ../scripts
