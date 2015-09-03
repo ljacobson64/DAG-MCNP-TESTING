b60108a64a952296cdc78d87d752ec299b5712a4
Lucas <ljacobson64@gmail.com>
2015-09-03 18:40:23 -0500

    added Meshtally to rebase_ref.sh

--- scripts/rebase_ref.sh
+++ scripts/rebase_ref.sh
@@ -27,19 +27,19 @@ cases_Meshtally[13]=uniform_flux
 cases_Meshtally[14]=uniform_vol_source
 
 for i in "${cases_Meshtally[@]}"; do
-  if [ -a cases/$i/ref/outp ];          then cp results/$i/test_o        cases/$i/ref/outp;          done
-  if [ -a cases/$i/ref/left_outp ];     then cp results/$i/test_o        cases/$i/ref/left_outp;     done
-  if [ -a cases/$i/ref/right_outp ];    then cp results/$i/test_o        cases/$i/ref/right_outp;    done
-  if [ -a cases/$i/ref/meshtal ];       then cp results/$i/meshtal       cases/$i/ref/meshtal;       done
-  if [ -a cases/$i/ref/left_meshtal ];  then cp results/$i/meshtal       cases/$i/ref/left_meshtal;  done
-  if [ -a cases/$i/ref/right_meshtal ]; then cp results/$i/meshtal       cases/$i/ref/right_meshtal; done
-  if [ -a cases/$i/ref/meshtal4.vtk ];  then cp results/$i/meshtal4.vtk  cases/$i/ref/meshtal4.vtk;  done
-  if [ -a cases/$i/ref/meshtal14.vtk ]; then cp results/$i/meshtal14.vtk cases/$i/ref/meshtal14.vtk; done
-  if [ -a cases/$i/ref/meshtal24.vtk ]; then cp results/$i/meshtal24.vtk cases/$i/ref/meshtal24.vtk; done
-  if [ -a cases/$i/ref/meshtal34.vtk ]; then cp results/$i/meshtal34.vtk cases/$i/ref/meshtal34.vtk; done
-  if [ -a cases/$i/ref/meshtal44.vtk ]; then cp results/$i/meshtal44.vtk cases/$i/ref/meshtal44.vtk; done
-  if [ -a cases/$i/ref/meshtal54.vtk ]; then cp results/$i/meshtal54.vtk cases/$i/ref/meshtal54.vtk; done
-  if [ -a cases/$i/ref/meshtal64.vtk ]; then cp results/$i/meshtal64.vtk cases/$i/ref/meshtal64.vtk; done
+  if [ -a cases/$i/ref/outp ];          then cp results/$i/test_o        cases/$i/ref/outp;          fi
+  if [ -a cases/$i/ref/left_outp ];     then cp results/$i/test_o        cases/$i/ref/left_outp;     fi
+  if [ -a cases/$i/ref/right_outp ];    then cp results/$i/test_o        cases/$i/ref/right_outp;    fi
+  if [ -a cases/$i/ref/meshtal ];       then cp results/$i/meshtal       cases/$i/ref/meshtal;       fi
+  if [ -a cases/$i/ref/left_meshtal ];  then cp results/$i/meshtal       cases/$i/ref/left_meshtal;  fi
+  if [ -a cases/$i/ref/right_meshtal ]; then cp results/$i/meshtal       cases/$i/ref/right_meshtal; fi
+  if [ -a cases/$i/ref/meshtal4.vtk ];  then cp results/$i/meshtal4.vtk  cases/$i/ref/meshtal4.vtk;  fi
+  if [ -a cases/$i/ref/meshtal14.vtk ]; then cp results/$i/meshtal14.vtk cases/$i/ref/meshtal14.vtk; fi
+  if [ -a cases/$i/ref/meshtal24.vtk ]; then cp results/$i/meshtal24.vtk cases/$i/ref/meshtal24.vtk; fi
+  if [ -a cases/$i/ref/meshtal34.vtk ]; then cp results/$i/meshtal34.vtk cases/$i/ref/meshtal34.vtk; fi
+  if [ -a cases/$i/ref/meshtal44.vtk ]; then cp results/$i/meshtal44.vtk cases/$i/ref/meshtal44.vtk; fi
+  if [ -a cases/$i/ref/meshtal54.vtk ]; then cp results/$i/meshtal54.vtk cases/$i/ref/meshtal54.vtk; fi
+  if [ -a cases/$i/ref/meshtal64.vtk ]; then cp results/$i/meshtal64.vtk cases/$i/ref/meshtal64.vtk; fi
 done
 
 # Regression test suite
